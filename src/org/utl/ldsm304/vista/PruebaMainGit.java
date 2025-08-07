447d0430dd99e0757de6ecc6770cbf0c350114ae
Daniela Diaz Hernandez <diazhernandezdaniela36@gmail.com>
2025-08-06 22:04:46 -0600

    Se agrega primer sout

---
+++ src/org/utl/ldsm304/vista/PruebaMainGit.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package org.utl.ldsm304.vista;
+
+/**
+ *
+ * @author USER
+ */
+public class PruebaMainGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola, soy Daniela Diaz");
+    }
+    
+}
